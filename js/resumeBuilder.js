c7c262fc7c9ab53debd8beee11885929a0aab850
Ben Colborn <ben@nutanix.com>
2016-12-16 17:46:02 -0800

    got the header. don't know why lines 20-21 are needed but page is blank without them

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,14 +4,19 @@ var resumeData =
 $.getJSON(resumeData, {
     format: "json"
 }).done(function (data) {
-    console.log(data.bio.role);
+    console.log(data.bio);
     
     var name = data.bio.name;
     var role = data.bio.role;
+    
+    var fmtName = HTMLheaderName.replace("%data%", name);
+    $("#header").append(fmtName);
     
     var fmtRole = HTMLheaderRole.replace("%data%", role);
     $("#header").append(fmtRole);
 });
 
+// why are these two lines needed?
 var fmtName = HTMLheaderName.replace("%data%", name);
 $("#header").append(fmtName);
+//***
