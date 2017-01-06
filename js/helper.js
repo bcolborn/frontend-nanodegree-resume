2edc14c5ca6a00ffbe6be794dd2d8452d41cc625
Ben Colborn <ben@nutanix.com>
2017-01-05 23:22:47 -0800

    color palette 18 from https://designschool.canva.com/blog/website-color-schemes/

--- js/helper.js
+++ js/helper.js
@@ -25,29 +25,29 @@ var HTMLskillsStart = '<h3 id="skills-h3">Skills</h3><ul id="skills" class="flex
 var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
-var HTMLworkEmployer = '<a href="#">%data%';
-var HTMLworkTitle = ' - %data%</a>';
+var HTMLworkEmployer = '<h3 class="title">%data%';
+var HTMLworkTitle = ' - %data%</h3>';
 var HTMLworkDates = '<div class="date-text">%data%</div>';
 var HTMLworkLocation = '<div class="location-text">%data%</div>';
 var HTMLworkDescription = '<p><br>%data%</p>';
 
 var HTMLprojectStart = '<div class="project-entry"></div>';
-var HTMLprojectTitle = '<a href="%url%">%data%</a>';
+var HTMLprojectTitle = '<h3><a href="%url%">%data%</a></h3>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
 var HTMLprojectImage = '<img src="%data%">';
 
 var HTMLpublicationStart = '<div class="project-entry"></div>';
-var HTMLpublicationTitle = '<a href="%url%">%data%</a>';
+var HTMLpublicationTitle = '<h3><a href="%url%">%data%</a></h3>';
 var HTMLpublicationDates = '<div class="date-text">%data%</div>';
 var HTMLpublicationDescription = '<p><br>%data%</p>';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = '<a href="#">%data%';
-var HTMLschoolDegree = ' - %data%</a>';
+var HTMLschoolName = '<h3>%data%';
+var HTMLschoolDegree = ' - %data%</h3>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
-var HTMLschoolLocation = '<div class="location-text">%data%</div>';
-var HTMLschoolMajor = '<em>Major: %data%</em>';
+var HTMLschoolLocation = '<div class="location-text">%data%</div><br/>';
+var HTMLschoolMajor = '<div class="major">Major: %data%</div>';
 
 var HTMLonlineClasses = '<h3>Online Classes</h3>';
 var HTMLonlineTitle = '<a href="#">%data%';
